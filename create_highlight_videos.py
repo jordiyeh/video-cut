64a247ce7a6a0b11e5939579d67e65ccf1f4c788
Jordi <jordiyeh@gmail.com>
2023-10-04 15:21:20 -0400

    Simplify usage create_highlight_videos.py
    Use labels from actual file for flexibility

--- create_highlight_videos.py
+++ create_highlight_videos.py
@@ -1,53 +1,72 @@
 import csv
 import getopt, sys
-from moviepy.editor import VideoFileClip, concatenate_videoclips
+from moviepy.video import fx
+from moviepy.editor import VideoFileClip, concatenate_videoclips, cvsecs, CompositeVideoClip, TextClip, vfx
+
+global folder
+global event
+global mp4_file
+global config_file
 
 folder = '/Videos/'
 # file name of the video and config file
-event = '20221002 PREECNLBVA'
-
-output_file = None # Create a file for each segment
-#output_file = 'check' # Compile the clips with a check flag
-output_file = 'highlight' # Compile the clips with a highligh flag
-#output_file = '20221002 EYF Segments.mp4' # compile all segments in the config file
-
-
-# --input
-mp4_file = folder + '/' + event + '.mp4'
-    # --config
-config_file = folder + '/' + event + '.csv'
-# --output
-
+event = 'my_sample'
 
 def return_filename(desc, prefix, suffix):
     return str(prefix or '') + str(desc or '') + str(suffix or '') + '.mp4'
 
+
+def add_freeze_frame(clip, row):
+    if 'add_freeze_sec' in row and int(row['add_freeze_sec']) > 0:
+        result = fx.all.freeze(clip, t=0, freeze_duration=int(row['add_freeze_sec']))
+
+        # Add description
+        if 'add_desc_to_video' in row and row['add_desc_to_video'][:1].lower() == 'y' and row['desc']:
+            txt_clip = TextClip(row['desc'], fontsize=50, color='white')
+            # setting position of text in the center and duration will be 10 seconds 
+            txt_clip = txt_clip.set_position(('left', 'bottom')).set_duration(int(row['add_freeze_sec']))
+            # Overlay the text clip on the first video clip 
+            result = CompositeVideoClip([result, txt_clip])
+
+            # Add minute and seconds
+        if 'add_time' in row and row['add_time'][:1].lower() == 'y':
+            txt_clip = TextClip(str(row['min']) + ':' + str(row['sec']), fontsize=50, color='white')
+            # setting position of text in the center and duration will be 10 seconds 
+            txt_clip = txt_clip.set_position(('right', 'bottom')).set_duration(int(row['add_freeze_sec']))
+            # Overlay the text clip on the first video clip 
+            result = CompositeVideoClip([result, txt_clip])
+
+        return result
+
+    else:
+        return clip
+
+
 def main():
     global folder
     global event
-    global output_file
     global mp4_file
     global config_file
 
-    argumentList = sys.argv[1:]
+    mp4_file = folder + '/' + event + '.mp4'
+    config_file = folder + '/' + event + '.csv'
+    argumentlist = sys.argv[1:]
 
-    options = "i:c:o:"
+    options = "i:c:"
 
-    long_options = ["input=","config=","output="]
+    long_options = ["input=", "config=", "output="]
+
+    output_file = None
 
     try:
-        arguments, values = getopt.getopt(argumentList, options, long_options)
-        for currentArgument, currentValue in arguments:
-            if currentArgument in ("-i", "--input"):
-                mp4_file = currentValue
-                # print ("File: ", currentValue)
-            if currentArgument in ("-o", "--output"):
-                output_file = currentValue
-            if currentArgument in ("-c", "--config"):
-                config_file = currentValue
-                # print ("Config: ", currentValue)
+        arguments, values = getopt.getopt(argumentlist, options, long_options)
+        for current_argument, current_value in arguments:
+            if current_argument in ("-i", "--input"):
+                mp4_file = current_value
+            if current_argument in ("-c", "--config"):
+                config_file = current_value
     except getopt.error as err:
-        print (str(err))
+        print(str(err))
 
     if mp4_file is None:
         # If mp4 file is not provided, use config file name
@@ -55,48 +74,40 @@ def main():
 
     # Read the config file
     rows = csv.DictReader(open(config_file))
+    fields = rows.fieldnames
 
-    first = True
-    for row in rows:
-        if row['source'] == 'video':
-            min = int(row['min'])
-            sec = int(row['sec'])
-            
-            if min > 0:
-                start_seconds = min * 60 + sec
-            else:
-                start_seconds = sec
-            length_in_sec = int(row['length_in_sec'])
-            end_seconds = start_seconds + length_in_sec
-            if start_seconds and end_seconds:
-                if output_file is None:
-                    # MODE = Split the segments into separate files
-                    clip = VideoFileClip(mp4_file).subclip(start_seconds, end_seconds)
-                    file_name = return_filename(row['desc'], row['filename_prefix'], row['filename_suffix'])
-                    clip.write_videofile(file_name)
+    # Get the distinct tags to s
+    s = set(())
+    for r in rows:
+        s.add(r['filename_suffix'].lower())
+
+    for keytext in s:
+        first = True
+        final_clip = None
+        rows = csv.DictReader(open(config_file))
+        for row in rows:
+            if row['source'] == 'video' and row["filename_suffix"].lower() == keytext:
+                min = int(row['min'])
+                sec = int(row['sec'])
+
+                if min > 0:
+                    start_seconds = min * 60 + sec
                 else:
-                    # MODE = Concatenate the segments into a single file
-                    if (output_file == 'check' and row['filename_suffix'] == 'check') or \
-                        (output_file == 'highlight' and row['filename_suffix'] == 'highlight') or \
-                            (output_file != 'check' and output_file != 'highlight'):
-                        # Save only if check or highlight or if all clips
-                        if first:
-                            final_clip = VideoFileClip(mp4_file).subclip(start_seconds, end_seconds)
-                            first = False
-                        else:
-                            clip = VideoFileClip(mp4_file).subclip(start_seconds, end_seconds)
-                            final_clip = concatenate_videoclips([final_clip,clip])
-            else:
-                print(f'Error with config settings for: {row}')
+                    start_seconds = sec
+                length_in_sec = int(row['length_in_sec'])
+                end_seconds = start_seconds + length_in_sec
+                if start_seconds and end_seconds:
+                    clip = VideoFileClip(mp4_file).subclip(start_seconds, end_seconds)
+                    fclip = add_freeze_frame(clip, row)
+                    if final_clip:
+                        final_clip = concatenate_videoclips([final_clip, fclip])
+                    else:
+                        final_clip = fclip
+                else:
+                    print(f'Error with config settings for: {row}')
 
-    if output_file:
-        # Save the final clip
-        if output_file == 'check':
-            output_file = event + ' check.mp4'
-        elif output_file == 'highlight':
-            output_file = event + ' highlight.mp4'
-        final_clip.write_videofile(output_file)
-    
+        final_clip.write_videofile(folder + "/" + event + ' ' + keytext + '.mp4', codec="libx264")
+
 
 if __name__ == "__main__":
     main()
